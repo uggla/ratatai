77cacadd5c2f0802844a3b2c0abfa40fc254ab1a
René Ribaud <rribaud@redhat.com>
2025-08-07 16:23:25 +0200

    feat: Add a logger to file

--- ratatai/src/main.rs
+++ ratatai/src/main.rs
@@ -2,10 +2,27 @@
 
 // Import everything public from our 'tui_app' crate (which will be defined in lib.rs)
 use ratatai::run;
+use tracing_subscriber::{EnvFilter, fmt, prelude::*};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Setup logging
+    let file_appender = tracing_appender::rolling::daily("logs", "ratatai.log");
+    let (non_blocking_appender, _guard) = tracing_appender::non_blocking(file_appender);
+
+    tracing_subscriber::registry()
+        .with(EnvFilter::from_default_env())
+        .with(fmt::layer().with_writer(non_blocking_appender))
+        .init();
+
+    tracing::info!("Application starting");
+
     // Call the main function of our application defined in lib.rs
-    run().await?;
+    let result = run().await;
+
+    tracing::info!("Application ending");
+
+    result?;
     Ok(())
 }
+
