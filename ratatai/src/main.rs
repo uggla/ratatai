8108446d7244137429ce03889cd693481139725b
René Ribaud <rribaud@redhat.com>
2025-08-08 15:40:47 +0200

    fix: logger to log as info if RUST_LOG is not set

--- ratatai/src/main.rs
+++ ratatai/src/main.rs
@@ -10,8 +10,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file_appender = tracing_appender::rolling::daily("logs", "ratatai.log");
     let (non_blocking_appender, _guard) = tracing_appender::non_blocking(file_appender);
 
+    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+
     tracing_subscriber::registry()
-        .with(EnvFilter::from_default_env())
+        .with(filter)
         .with(fmt::layer().with_writer(non_blocking_appender))
         .init();
 
@@ -25,4 +27,3 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     result?;
     Ok(())
 }
-
